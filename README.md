23e6214c9653cc444d3bda5097dab7b69968f17e
Tnze <cjd001113@outlook.com>
2021-12-11 15:22:19 +0800

    dfs algo

---
+++ README.md
@@ -0,0 +1,5 @@
+# FFXIV-Crafting-Algo
+
+An example usage of https://crates.io/crates/ffxiv-crafting
+
+用于暴力搜索FF14生产宏的小工具。
---
+++ src/main.rs
@@ -0,0 +1,150 @@
+use ffxiv_crafting::{export, Attributes, Recipe, Skills, Status};
+use std::cmp::Ordering;
+
+fn main() {
+    let attributes = Attributes {
+        level: 80,
+        craftsmanship: 2806,
+        control: 2818,
+        craft_points: 557,
+    };
+    let recipe = Recipe::new(412, 77, 3586, 15742, 80, 15);
+    let status = Status::new(attributes, recipe);
+    let (best_list, Score { quality, steps }) = dfs_search(&status, 7);
+    println!(
+        "{}# 品质：{}，步数：{}",
+        export::to_chinese_macro(&best_list),
+        quality,
+        steps,
+    );
+}
+
+fn dfs_search(status: &Status, maximum_depth: i32) -> (Vec<Skills>, Score) {
+    let mut best_list = Vec::new();
+    let mut best_score = Score::from(status);
+    search(
+        &status,
+        0,
+        maximum_depth,
+        &mut Vec::with_capacity(maximum_depth as usize),
+        &mut best_list,
+        &mut best_score,
+    );
+    fn search(
+        status: &Status,
+        current_depth: i32,
+        maximum_depth: i32,
+        stack_seq: &mut Vec<Skills>,
+        best_seq: &mut Vec<Skills>,
+        best_score: &mut Score,
+    ) {
+        let score = Score::from(status);
+        if score > *best_score {
+            *best_score = score;
+            *best_seq = stack_seq.clone();
+        }
+        let is_best_quality_full = best_score.quality >= status.recipe.quality;
+        let is_this_steps_longer = current_depth >= best_score.steps;
+        if is_best_quality_full && is_this_steps_longer || current_depth > maximum_depth {
+            return;
+        }
+        for sk in SKILL_LIST {
+            let mut new_s = *status;
+            if new_s.is_action_allowed(sk).is_ok() {
+                new_s.cast_action(sk);
+                stack_seq.push(sk);
+                search(
+                    &new_s,
+                    current_depth + 1,
+                    maximum_depth,
+                    stack_seq,
+                    best_seq,
+                    best_score,
+                );
+                stack_seq.pop();
+            }
+        }
+    }
+    (best_list, best_score)
+}
+
+#[derive(Copy, Clone, Eq)]
+struct Score {
+    quality: i32,
+    steps: i32,
+}
+
+impl From<&Status> for Score {
+    fn from(s: &Status) -> Self {
+        Self {
+            quality: if s.progress >= s.recipe.progress {
+                s.quality.min(s.recipe.quality)
+            } else {
+                0
+            },
+            steps: s.step,
+        }
+    }
+}
+
+impl Ord for Score {
+    /// 定义Score之间的偏序关系，这里不使用Ord是因为避免麻烦
+    /// * 品质更高的分数更高
+    /// * 品质相同，步数越短分数越高
+    /// * 品质和步数都相同，两个分数相等
+    fn cmp(&self, other: &Self) -> Ordering {
+        match self.quality.cmp(&other.quality) {
+            Ordering::Equal => self.steps.cmp(&other.steps).reverse(),
+            x => x,
+        }
+    }
+}
+
+impl PartialOrd<Self> for Score {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq<Self> for Score {
+    fn eq(&self, other: &Self) -> bool {
+        matches!(self.cmp(other), Ordering::Equal)
+    }
+}
+
+/// 搜索的技能列表
+#[allow(dead_code)]
+const SKILL_LIST: [Skills; 24] = [
+    Skills::MuscleMemory,
+    Skills::Reflect,
+    // Skills::TrainedEye,
+    Skills::BasicSynthesis,
+    // Skills::RapidSynthesis,
+    Skills::BrandOfTheElements,
+    Skills::CarefulSynthesis,
+    // Skills::FocusedSynthesis,
+    Skills::Groundwork,
+    // Skills::IntensiveSynthesis,
+    Skills::DelicateSynthesis,
+    Skills::BasicTouch,
+    // Skills::HastyTouch,
+    Skills::StandardTouch,
+    Skills::ByregotsBlessing,
+    // Skills::PreciseTouch,
+    // Skills::PatientTouch,
+    Skills::PrudentTouch,
+    // Skills::FocusedTouch,
+    Skills::PreparatoryTouch,
+    Skills::TricksOfTheTrade,
+    Skills::MastersMend,
+    Skills::WasteNot,
+    Skills::WasteNotII,
+    Skills::Manipulation,
+    Skills::InnerQuiet,
+    Skills::Veneration,
+    Skills::GreatStrides,
+    Skills::Innovation,
+    Skills::NameOfTheElements,
+    Skills::Observe,
+    Skills::FinalAppraisal,
+];
